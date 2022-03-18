23ed93ced5d4a50a317958e1df9a6e1cf36a6351
young <dudghk1207@gmail.com>
2022-03-18 22:39:30 +0900

    duplication eliminate

--- excel.js
+++ excel.js
@@ -1,52 +1,47 @@
 const xlsx = require( "xlsx" );
 var fs = require('fs');
 
-
-// @files 엑셀 파일을 가져온다.
-
 const excel = xlsx.readFile( "pluto.xlsx" );
-
-
-
-// @breif 엑셀 파일의 첫번째 시트의 정보를 추출
-
 const sheet_name = excel.SheetNames[0];          
 const sheet_data = excel.Sheets[sheet_name];     
 
 // let json = xlsx.utils.sheet_to_json( sheet_data, { defval : "" } );
-
 let json = xlsx.utils.sheet_to_json( sheet_data );
 
-let id_set=[];
-for (let i =1;i<json.length;i++)
+let duplication_eliminate = (json) =>
 {
-    id_set.push(json[i].id);  
+    let id_set=[];
+    for (let i =1;i<json.length;i++)
+    {
+        id_set.push(json[i].id);  
+    }
+    let set= new Set(id_set);
+    id_set = [...set];
+
+    let json_unique=[];
+    for (let i=0;i<id_set.length;i++)
+    {
+        let j=1;
+        while (true)
+        {   
+            if(id_set[i] == json[j].id )
+            {
+                json_unique.push(json[j]);
+                j=1;
+                break;
+            }
+            j++;
+        }
+    }
+    return json_unique;
 }
-let set= new Set(id_set);
-id_set = [...set];
 
-let duplication_check=[];
-for (let i=0;i<id_set.length;i++)
-{
-    let j=1;
-   while (true)
-   {   
-       if(id_set[i] == json[j].id )
-       {
-         duplication_check.push(json[j]);
-         j=1;
-         break;
-       }
-       j++;
-   }
-}
-
-json = duplication_check;
-
-
+json = duplication_eliminate(json);
 json = JSON.stringify(json);
+
 fs.writeFile("test.json", json , function(err) {
     if (err) {
         console.log(err);
     }
 });
+
