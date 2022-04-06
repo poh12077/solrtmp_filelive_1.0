59a886d418f4edebdf5cad2b728f8f98089affab
young <dudghk1207@gmail.com>
2022-04-06 14:26:27 +0900

    fix

--- solRTMP_filelive_monitoring.js
+++ solRTMP_filelive_monitoring.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 
-let data = fs.readFileSync('test_solrtmp_pluto.log', 'utf8');
-//let data = fs.readFileSync('test_solrtmp_samsung.log', 'utf8');
+//let data = fs.readFileSync('test_solrtmp_pluto.log', 'utf8');
+let data = fs.readFileSync('test_solrtmp_samsung.log', 'utf8');
 
 let full_log =[];
 full_log = data.split('\n');
@@ -20,17 +20,21 @@ let log_list = [];
 
 for (let i=0;i<full_log.length;i++)
 {
-    let index = full_log[i].indexOf('play');
+    let index = full_log[i].indexOf(' play=');
     if(index!=-1)
     {
         let time = full_log[i].substr(0,19);
         let channel_id =  full_log[i].substr( full_log[i].indexOf('(id=') ).split('/')[0].substr(4);
         let video_id = full_log[i].substr( full_log[i].indexOf('(main:') ).split('/')[0].substr(6);
+       
         log_list.push( new log(time, channel_id, video_id) );
     }
 }
 
-
+for(let i=0;i<log_list.length;i++)
+{
+    console.log(log_list[i].time + " " + log_list[i].channel_id + ' ' + log_list[i].video_id);
+}
 
 
 
