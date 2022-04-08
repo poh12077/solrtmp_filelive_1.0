96a83df0a13c9f594851d9c6f92b0b03f99c6c72
young <dudghk1207@gmail.com>
2022-04-09 00:25:23 +0900

    schedule list

--- solRTMP_filelive_monitoring.js
+++ solRTMP_filelive_monitoring.js
@@ -1,36 +1,44 @@
 var fs = require('fs');
 
-let data = fs.readFileSync('test_solrtmp_pluto.log', 'utf8');
+let file = fs.readFileSync('test_solrtmp_pluto.log', 'utf8');
 //let data = fs.readFileSync('test_solrtmp_samsung.log', 'utf8');
 
 let full_log = [];
-full_log = data.split('\n');
+full_log = file.split('\n');
 
-class log {
-    constructor(time, channel_id, video_id) {
-        this.time = time;
-        this.channel_id = channel_id;
+let log ={
+
+}
+
+class line{
+    constructor(time,video_id){
+        this.time =time;
         this.video_id = video_id;
     }
 }
 
-let log_list = [];
+let channel_list=[];
 
 for (let i = 0; i < full_log.length; i++) {
     let index = full_log[i].indexOf(' play=');
     if (index != -1) {
         let time = full_log[i].substr(0, 19);
         let channel_id = full_log[i].substr(full_log[i].indexOf('(id=')).split('/')[0].substr(4);
+        if(  !(channel_list.includes(channel_id)) )
+        {
+           channel_list.push(channel_id);
+           log[channel_id]=[];
+        }      
         let video_id = full_log[i].substr(full_log[i].indexOf('(main:')).split('/')[0].substr(6);
 
-        log_list.push(new log(time, channel_id, video_id));
+        log[channel_id].push(new line(time, video_id));
     }
 }
 
-for (let i = 0; i < log_list.length; i++) {
-    console.log(log_list[i].time + " " + log_list[i].channel_id + ' ' + log_list[i].video_id);
+for (let x in log)
+{
+    for(let i=0;i<log[x].length;i++)
+    {
+        console.log(x + ' ' + log[x][i].time + ' ' + log[x][i].video_id);
+    }
 }
-
-
-
-
