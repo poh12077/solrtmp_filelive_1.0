e4ae0bc0a29fad90425f60a433b7e100425ec1cf
young <dudghk1207@gmail.com>
2022-04-11 22:35:40 +0900

    unix time

--- solRTMP_filelive_monitoring_excel.js
+++ solRTMP_filelive_monitoring_excel.js
@@ -1,17 +1,37 @@
 const xlsx = require("xlsx");
 var fs = require('fs');
 
+// class video_info {
+//     constructor(id, end_time, ad_point_1, ad_point_2, ad_point_3, ad_point_4, ad_point_5) {
+//         this.id = id;
+//         this.end_time = end_time;
+//         this.ad_point_1 = ad_point_1;
+//         this.ad_point_2 = ad_point_2;
+//         this.ad_point_3 = ad_point_3;
+//         this.ad_point_4 = ad_point_4;
+//         this.ad_point_5 = ad_point_5;
+//     }
+// }
+
 class video_info {
-    constructor(id, playtime, ad_point_1, ad_point_2, ad_point_3, ad_point_4, ad_point_5) {
+    constructor(id, end_time) {
         this.id = id;
-        this.playtime = playtime;
-        this.ad_point_1 = ad_point_1;
-        this.ad_point_2 = ad_point_2;
-        this.ad_point_3 = ad_point_3;
-        this.ad_point_4 = ad_point_4;
-        this.ad_point_5 = ad_point_5;
+        this.end_time = end_time;
     }
 }
+
+
+let advertisement={
+    start:'',
+    end:''
+}
+
+//time =　'2012-05-17 10:20:30'　
+let fetch_unix_timestamp = (time) =>
+{
+	return Math.floor(new Date(time).getTime() / 1000);
+}
+
 
 let read_conf = (file_name) => {
     try {
@@ -26,7 +46,7 @@ let read_conf = (file_name) => {
 
         conf.file_name = conf_file.file_name;
         conf.option = conf_file.option;
-        conf.start_date = conf_file.start_date;
+        conf.start_date = fetch_unix_timestamp(conf_file.start_date);
 
         if (conf.option < 1 || conf.option > 4) {
             throw new Error("[error] configure value");
@@ -53,17 +73,21 @@ let read_excel = (excel, i) => {
     }
 }
 
-let parser = (json) => {
+
+let parser = (json, conf) => {
     let schedule = [];
     let video;
+    let end_time = conf.start_date ;
     for (let i = 0; i < json.length; i++) {
         if (json[i].id !== undefined) {
-            video = new video_info(json[i]['id'], json[i]['__EMPTY'], json[i]['Ad Point 1'], json[i]['Ad Point 2'], json[i]['Ad Point 3'], json[i]['Ad Point 4'], json[i]['Ad Point 5']);
+            end_time += json[i]['__EMPTY']; 
+            video = new video_info(json[i]['id'], end_time );
             schedule.push(video);
         }
     }
     return schedule;
 }
+
 
 let id_finder =(current_time) =>
 {
@@ -87,7 +111,7 @@ let main = () => {
     let json;
     for (let i = 0; i < excel.SheetNames.length; i++) {
         json = read_excel(excel, i);
-        schedule = parser(json);
+        schedule = parser(json, conf);
     }
 }
 
